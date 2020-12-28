da1fa9a292093551fd894a31bad0770d7114b455
ExtraHash <ExtraHash@protonmail.com>
2020-12-28 11:04:46 -0700

    move save keyfile and load keyfile into crypto pakage

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { XTypes } from "@vex-chat/types";
 import * as bip39 from "bip39";
 import createHmac from "create-hmac";
 import ed2curve from "ed2curve";
+import fs from "fs";
 import hkdf from "futoin-hkdf";
 import msgpack from "msgpack-lite";
+import { pbkdf2Sync } from "pbkdf2";
 import nacl from "tweetnacl";
 
 export const XKeyConvert = ed2curve;
@@ -53,6 +55,68 @@ export class XUtils extends KeyRingUtils {
     public static emptyHeader() {
         return new Uint8Array(xConstants.HEADER_SIZE);
     }
+
+    public static saveKeyFile = (
+        path: string,
+        password: string,
+        keyToSave: string
+    ): void => {
+        const UNENCRYPTED_SIGNKEY = XUtils.decodeHex(keyToSave);
+
+        // generate random amount of iterations
+        const R1 = nacl.randomBytes(1);
+        const R2 = nacl.randomBytes(1);
+        const N1 = XUtils.uint8ArrToNumber(R1);
+        const N2 = XUtils.uint8ArrToNumber(R2);
+        const iterations = N1 * N2;
+
+        // length 6
+        const ITERATIONS = XUtils.numberToUint8Arr(iterations);
+
+        // length 24
+        const PKBDF_SALT = xMakeNonce();
+
+        // derived key to encrypt our signkeys with
+        const ENCRYPTION_KEY = Uint8Array.from(
+            pbkdf2Sync(password, PKBDF_SALT, iterations, 32)
+        );
+        const NONCE = xMakeNonce();
+
+        const ENCRYPTED_SIGNKEY = nacl.secretbox(
+            UNENCRYPTED_SIGNKEY,
+            NONCE,
+            ENCRYPTION_KEY
+        );
+        fs.writeFileSync(
+            path,
+            xConcat(ITERATIONS, PKBDF_SALT, NONCE, ENCRYPTED_SIGNKEY)
+        );
+    };
+
+    public static loadKeyFile = (path: string, password: string): string => {
+        const keyFile = Uint8Array.from(fs.readFileSync(path));
+        const ITERATIONS = XUtils.uint8ArrToNumber(keyFile.slice(0, 6));
+        const PKBDF_SALT = keyFile.slice(6, 30);
+        const ENCRYPTION_NONCE = keyFile.slice(30, 54);
+        // this is the id key we need to decrypt
+        const ENCRYPTED_KEY = keyFile.slice(54);
+        // the derived key from the user's password
+        const DERIVED_KEY = Uint8Array.from(
+            pbkdf2Sync(password, PKBDF_SALT, ITERATIONS, 32)
+        );
+
+        const DECRYPTED_SIGNKEY = nacl.secretbox.open(
+            ENCRYPTED_KEY,
+            ENCRYPTION_NONCE,
+            DERIVED_KEY
+        );
+
+        if (!DECRYPTED_SIGNKEY) {
+            throw new Error("Decryption failed. Wrong password?");
+        } else {
+            return XUtils.encodeHex(DECRYPTED_SIGNKEY);
+        }
+    };
 
     /**
      * Decodes a hex string into a Uint8Array.
